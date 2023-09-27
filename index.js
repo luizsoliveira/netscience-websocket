3c7e53195363dfcd912af76f6445301d29a7ccc0
Luiz Oliveira <luizsoliveira@gmail.com>
2023-09-26 23:13:36 -0700

    Improves on websocket server

--- index.js
+++ index.js
@@ -2,11 +2,26 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+
+// Call the express framework
+// Thanks @Farias-sys
+const cors = require('cors')
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:7000")
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    app.use(cors())
+    next()
+})
 
 //Setting Socket.io
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(httpServer,{
+  cors: {
+    origin: "http://localhost:7000"     
+  }
+});
 
 //Setting the filesystem libraries
 const fs = require('fs');
@@ -40,38 +55,38 @@ app.get('/stdout_segment/:taskId/:begin/:end', (req, res) => {
 */
 //io.of(/^\/file_monitor-\w+$/).on("connection", (socket) => {
 io.of(/^\/stdout_monitor-[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/).on("connection", (socket) => {
-    const namespace = socket.nsp;
-    const taskId = namespace.name.split('stdout_monitor-', 2)[1]
+    const namespace = socket.nsp.name;
+    const taskId = namespace.split('stdout_monitor-', 2)[1]
 
     if (taskId == null || taskId == undefined) {
+      console.warn('Aborting. TaskId is a obligatory parameter.')
       return
     }
 
     const filename = `/var/tasks/${taskId}/stdout.log`
 
-    console.log(`someone connected to file monitor namespace: ${namespace.name} taskId: ${taskId}`);
+    console.log(`someone connected to file monitor namespace: ${namespace} taskId: ${taskId}`);
 
-    //Detecting that the client is ready to receive the existent number of lines of the file
-    const numberOfLines = fileUtils.getNumberOfLines(filename)
-    socket.emit('file_update', {
-        message: 'File changed',
-        taskId: taskId,
-        number_of_lines: numberOfLines
-    });
+    socket.on('hello', (msg) => {
+      console.log(`Ping received on the server`)
+      const numberOfLines = fileUtils.getNumberOfLines(filename)
+      socket.emit('lastLine', numberOfLines);
+
+    })
 
     //Creating listener that will monitor the file changes
-    fs.watchFile(filename, {persistent:true, interval: 4000}, function(data) {
+    fs.watchFile(filename, {persistent:true, interval: 1000}, function(data) {
         const numberOfLines = fileUtils.getNumberOfLines(filename)
-        console.log(`File update detected: ${filename} numberLines: ${numberOfLines}`)
-        socket.emit('file_update', {
-            message: 'File changed',
-            taskId: taskId,
-            number_of_lines: numberOfLines
-        });
+        //console.log(`File update detected: ${filename} numberLines: ${numberOfLines}`)
+        socket.emit('lastLine', numberOfLines);
+
     });
 
   });
 
-server.listen(3000, () => {
+  
+  
+
+httpServer.listen(3000, () => {
   console.log('listening on *:3000');
 });
